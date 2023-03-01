4d6ddb805fa74f24ef50a20768c021b31d374ea9
LX-sys <lx984608061@163.com>
2023-03-01 17:05:02 +0800

    多文件编程c

---
+++ Mylib_C/mySystem.c
@@ -0,0 +1,36 @@
+//
+// Created by Administrator on 2023/3/1.
+//
+#include <stdio.h>
+#include <stdlib.h>
+#define bool int
+
+// 判断操作系统
+bool isSystem(){
+#ifdef _WIN32
+    #ifdef _WIN64
+            printf("win64 system\n");
+            return 64;
+        #else
+            printf("win32 system\n");
+            return 32;
+        #endif
+#elif __MACH__
+    printf("MacOS system\n");
+#endif
+    return 0;
+}
+
+
+// 处理中文
+int processingChinese(){
+    /*
+     * 处理CLion在Win下使用WinGW作为编译器时的中文问题
+     *
+     * */
+    if(isSystem() >=32){
+        // 这句话是在 win下使用WinGW编译器的时候支持中文输出
+        system("chcp 65001");
+    }
+    return 0;
+}
