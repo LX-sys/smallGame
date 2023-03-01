143c303eeee911f51a748b1ae8feb0542c7ff760
LX-sys <lx984608061@163.com>
2023-03-01 17:05:21 +0800

    多文件编程h

---
+++ Mylib_H/mySystem.h
@@ -0,0 +1,12 @@
+//
+// Created by Administrator on 2023/3/1.
+//
+
+#ifndef SMALLGAME_MYSYSTEM_H
+#define SMALLGAME_MYSYSTEM_H
+#define bool int
+
+bool isSystem();  // 判断操作系统(Win/MasOS)
+bool processingChinese(); // 处理Win下的中文乱码情况
+
+#endif //SMALLGAME_MYSYSTEM_H
