97ba7abaa5ca2399e107268c86d3945af02f9b90
LX-sys <lx984608061@163.com>
2023-03-04 16:25:25 +0800

    更新

--- main.c
+++ main.c
@@ -3,26 +3,9 @@
 #include "Mylib_H/mySystem.h"
 #include "Mylib_H/module.h"
 
-int main() {
-    // 处理中文
-    processingChinese();
-    // ---
 
-    const float PL = 3.14f;
-    char names[] = "Liu Xuan";
-    float decimal = 3.14f;
-    printf("PL=%0.2f\n",PL);
-    printf("my name is \"%s\"\n",names);
-    printf("2^5=%d\n", (int)pow(2,5));
-    printf("小数:%0.2f\n",decimal);
-
-    char inputName[20];
-    printf("输入名称:");
-    fgets(inputName,20,stdin);
-    printf("名字是:%s\n",inputName);
-
-    // -----以上是学习内容
-
+// 初始化
+void Init(){
     int options;
 
     isSystem();
@@ -47,6 +30,29 @@ int main() {
         }
     }
     printf("程序运行结束\n");
+}
+
+int main() {
+    // 处理中文
+    processingChinese();
+    // ---
+
+    const float PL = 3.14f;
+    char names[] = "Liu Xuan";
+    float decimal = 3.14f;
+    printf("PL=%0.2f\n",PL);
+    printf("my name is \"%s\"\n",names);
+    printf("2^5=%d\n", (int)pow(2,5));
+    printf("小数:%0.2f\n",decimal);
+
+    char inputName[20];
+    printf("输入名称:");
+    fgets(inputName,20,stdin);
+    printf("名字是:%s\n",inputName);
+
+    // -----以上是学习内容
+
+    Init();
     return 0;
 }
 
